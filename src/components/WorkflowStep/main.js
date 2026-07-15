0074f0fc0eda8729f291c52f17141ee7c6fa8379
sukhmandeep kaur <sukhmandeepkaur12345@gmail.com>
2026-07-15 12:17:09 +0530

    workflowstep reformation updated

--- src/components/WorkflowStep/main.js
+++ src/components/WorkflowStep/main.js
@@ -75,13 +75,13 @@ export default function WorkflowStep() {
   const [showModal, setShowModal] = useState(false);
 
   return (
-    <div className="p-6 bg-slate-100 min-h-screen font-sans text-slate-700">
+    <div className="p-6 bg-[#f5f7fb] min-h-screen font-sans text-slate-700">
       <div className="bg-white rounded-2xl shadow-md overflow-hidden">
         {/* Header */}
         <div className="px-6 py-5 border-b border-gray-200">
-          <h2 className="m-0 text-[30px] leading-tight font-semibold text-[#1E293B] tracking-tight">
+          <h1 className="m-0 text-[30px] leading-tight font-semibold text-[#1E293B] tracking-tight">
             Workflow Step
-          </h2>
+          </h1>
 
           <div className="mt-2 text-[13px] font-medium">
             <a href="/dashboard" className="text-blue-600 hover:underline">
@@ -107,12 +107,12 @@ export default function WorkflowStep() {
         </div>
 
         {/* Search Section */}
-        <div className="px-6 py-5 border-b border-gray-200 bg-gray-50">
+        <div className="px-6 py-5 border-b border-gray-200 ">
           <div className="flex justify-between items-center flex-wrap gap-3">
             <input
               type="text"
               placeholder="Search workflow step..."
-              className="w-[300px] px-4 py-2.5 border border-gray-300 rounded-lg text-[13px] text-slate-600 focus:outline-none"
+              className="w-[300px] px-4 py-2.5 border border-slate-200/80 rounded-lg text-[13px] text-slate-600 focus:outline-none bg-slate-50"
             />
 
             <button
@@ -158,47 +158,49 @@ export default function WorkflowStep() {
                   key={item.workflowStepId}
                   className="hover:bg-slate-50 transition-colors"
                 >
-                  <td className="px-4 py-3 border-b border-gray-200 text-[13px] text-[#334155] font-bold">
+                  <td className="px-4 py-3 border-b border-slate-200 text-[13px] text-slate-500 font-bold">
                     {item.workflowStepId}
                   </td>
 
-                  <td className="px-4 py-3 border-b border-gray-200 text-[13px] text-[#334155]  font-bold">
+                  <td className="px-4 py-3 border-b border-slate-200 text-[13px] text-slate-500 font-bold">
                     {item.workflowId}
                   </td>
 
-                  <td className="px-4 py-3 border-b border-gray-200 text-[13px] text-[#334155]  font-bold">
+                  <td className="px-4 py-3 border-b border-slate-200 text-[13px] text-slate-500 font-bold">
+                    <span className="inline-flex item-center px-3 py-1 rounded-md bg-[#f1f5f9] text-[#334155] text-xs font-semibold border border-[#e2e8f0]">
                     {item.stepCode}
+                    </span>
                   </td>
 
-                  <td className="px-4 py-3 border-b border-gray-200 text-[13px] text-[#334155]  font-bold">
+                  <td className="px-4 py-3 border-b border-slate-200 text-[13px] text-slate-500 font-bold">
                     {item.stepName}
                   </td>
 
-                  <td className="px-4 py-3 border-b border-gray-200 text-[13px] text-[#334155]  font-bold">
+                  <td className="px-4 py-3 border-b border-slate-200 text-[13px] text-slate-500 font-bold">
                     {item.sequenceNo}
                   </td>
 
-                  <td className="px-4 py-3 border-b border-gray-200 text-[13px] text-[#334155]  font-bold">
+                  <td className="px-4 py-3 border-b border-slate-200 text-[13px] text-slate-500 font-bold">
                     {item.slaType}
                   </td>
 
-                  <td className="px-4 py-3 border-b border-gray-200 text-[13px] text-[#334155]  font-bold">
+                  <td className="px-4 py-3 border-b border-slate-200 text-[13px] text-slate-500 font-bold">
                     {item.slaValue}
                   </td>
 
-                  <td className="px-4 py-3 border-b border-gray-200 text-[13px] text-[#334155] font-bold">
+                  <td className="px-4 py-3 border-b border-slate-200 text-[13px] text-slate-500 font-bold">
                     {item.reminderBefore}
                   </td>
 
-                  <td className="px-4 py-3 border-b border-gray-200 text-[13px] text-[#334155] font-bold">
+                  <td className="px-4 py-3 border-b border-slate-200 text-[13px] text-slate-500 font-bold">
                     {item.escalationAfter}
                   </td>
 
-                  <td className="px-4 py-3 border-b border-gray-200 text-[13px] text-[#334155] font-bold">
+                  <td className="px-4 py-3 border-b border-slate-200 text-[13px] text-slate-500 font-bold">
                     {item.workingHoursOnly}
                   </td>
 
-                  <td className="px-4 py-3 border-b border-gray-200 text-[13px] text-[#334155] font-bold">
+                  <td className="px-4 py-3 border-b border-slate-200 text-[13px] text-slate-500 font-bold">
                     {item.excludeHolidays}
                   </td>
                 </tr>
